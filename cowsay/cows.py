af3747c1ad97b51ef86c788a7b0243638b50022e
Ovlic <74332950+Ovlic@users.noreply.github.com>
2022-02-10 13:03:21 -0800

    Add @property for cows

--- cowsay/cows.py
+++ cowsay/cows.py
@@ -192,194 +192,576 @@ from .lib.cows.zen_noh_milk import Zen_noh_milk
 
 class cows:
     """cows yay"""
+    
+    @property
     def C3PO(**kwargs): return C3PO(**kwargs)
+    
+    @property
     def R2_D2(**kwargs): return R2_D2(**kwargs)
+    
+    @property
     def USA(**kwargs): return USA(**kwargs)
+    
+    @property
     def ackbar(**kwargs): return Ackbar(**kwargs)
+    
+    @property
     def aperture(**kwargs): return Aperture(**kwargs)
+    
+    @property
     def aperture_blank(**kwargs): return Aperture_blank(**kwargs)
+    
+    @property
     def armadillo(**kwargs): return Armadillo(**kwargs)
+    
+    @property
     def atat(**kwargs): return Atat(**kwargs)
+    
+    @property
     def atom(**kwargs): return Atom(**kwargs)
+    
+    @property
     def awesome_face(**kwargs): return Awesome_face(**kwargs)
+    
+    @property
     def banana(**kwargs): return Banana(**kwargs)
+    
+    @property
     def bearface(**kwargs): return Bearface(**kwargs)
+    
+    @property
     def beavis_zen(**kwargs): return Beavis_zen(**kwargs)
+    
+    @property
     def bees(**kwargs): return Bees(**kwargs)
+    
+    @property
     def bill_the_cat(**kwargs): return Bill_the_cat(**kwargs)
+    
+    @property
     def biohazard(**kwargs): return Biohazard(**kwargs)
+    
+    @property
     def bishop(**kwargs): return Bishop(**kwargs)
+    
+    @property
     def black_mesa(**kwargs): return Black_mesa(**kwargs)
+    
+    @property
     def bong(**kwargs): return Bong(**kwargs)
+    
+    @property
     def box(**kwargs): return Box(**kwargs)
+    
+    @property
     def broken_heart(**kwargs): return Broken_heart(**kwargs)
+    
+    @property
     def bud_frogs(**kwargs): return Bud_frogs(**kwargs)
+    
+    @property
     def bunny(**kwargs): return Bunny(**kwargs)
+    
+    @property
     def cake(**kwargs): return Cake(**kwargs)
+    
+    @property
     def cake_with_candles(**kwargs): return Cake_with_candles(**kwargs)
+    
+    @property
     def cat(**kwargs): return Cat(**kwargs)
+    
+    @property
     def cat2(**kwargs): return Cat2(**kwargs)
+    
+    @property
     def catfence(**kwargs): return Catfence(**kwargs)
+    
+    @property
     def charizardvice(**kwargs): return Charizardvice(**kwargs)
+    
+    @property
     def charlie(**kwargs): return Charlie(**kwargs)
+    
+    @property
     def cheese(**kwargs): return Cheese(**kwargs)
+    
+    @property
     def chessmen(**kwargs): return Chessmen(**kwargs)
+    
+    @property
     def chito(**kwargs): return Chito(**kwargs)
+    
+    @property
     def claw_arm(**kwargs): return Claw_arm(**kwargs)
+    
+    @property
     def clippy(**kwargs): return Clippy(**kwargs)
+    
+    @property
     def companion_cube(**kwargs): return Companion_cube(**kwargs)
+    
+    @property
     def cower(**kwargs): return Cower(**kwargs)
+    
+    @property
     def cowfee(**kwargs): return Cowfee(**kwargs)
+    
+    @property
     def cthulhu_mini(**kwargs): return Cthulhu_mini(**kwargs)
+    
+    @property
     def cube(**kwargs): return Cube(**kwargs)
+    
+    @property
     def daemon(**kwargs): return Daemon(**kwargs)
+    
+    @property
     def dalek(**kwargs): return Dalek(**kwargs)
+    
+    @property
     def dalek_shooting(**kwargs): return Dalek_shooting(**kwargs)
+    
+    @property
     def default(**kwargs): return Default(**kwargs)
+    
+    @property
     def defaultCow(**kwargs): return Default(**kwargs)
+    
+    @property
     def docker_whale(**kwargs): return Docker_whale(**kwargs)
+    
+    @property
     def doge(**kwargs): return Doge(**kwargs)
+    
+    @property
     def dolphin(**kwargs): return Dolphin(**kwargs)
+    
+    @property
     def dragon(**kwargs): return Dragon(**kwargs)
+    
+    @property
     def dragon_and_cow(**kwargs): return Dragon_and_cow(**kwargs)
+    
+    @property
     def ebi_furai(**kwargs): return Ebi_furai(**kwargs)
+    
+    @property
     def elephant(**kwargs): return Elephant(**kwargs)
+    
+    @property
     def elephant2(**kwargs): return Elephant2(**kwargs)
+    
+    @property
     def elephant_in_snake(**kwargs): return Elephant_in_snake(**kwargs)
+    
+    @property
     def explosion(**kwargs): return Explosion(**kwargs)
+    
+    @property
     def eyes(**kwargs): return Eyes(**kwargs)
+    
+    @property
     def fat_banana(**kwargs): return Fat_banana(**kwargs)
+    
+    @property
     def fat_cow(**kwargs): return Fat_cow(**kwargs)
+    
+    @property
     def fence(**kwargs): return Fence(**kwargs)
+    
+    @property
     def fire(**kwargs): return Fire(**kwargs)
+    
+    @property
     def flaming_sheep(**kwargs): return Flaming_sheep(**kwargs)
+    
+    @property
     def fox(**kwargs): return Fox(**kwargs)
+    
+    @property
     def ghost(**kwargs): return Ghost(**kwargs)
+    
+    @property
     def ghostbusters(**kwargs): return Ghostbusters(**kwargs)
+    
+    @property
     def glados(**kwargs): return Glados(**kwargs)
+    
+    @property
     def goat(**kwargs): return Goat(**kwargs)
+    
+    @property
     def goat2(**kwargs): return Goat2(**kwargs)
+    
+    @property
     def golden_eagle(**kwargs): return Golden_eagle(**kwargs)
+    
+    @property
     def hand(**kwargs): return Hand(**kwargs)
+    
+    @property
     def happy_whale(**kwargs): return Happy_whale(**kwargs)
+    
+    @property
     def hedgehog(**kwargs): return Hedgehog(**kwargs)
+    
+    @property
     def hellokitty(**kwargs): return Hellokitty(**kwargs)
+    
+    @property
     def hippie(**kwargs): return Hippie(**kwargs)
+    
+    @property
     def hiya(**kwargs): return Hiya(**kwargs)
+    
+    @property
     def hiyoko(**kwargs): return Hiyoko(**kwargs)
+    
+    @property
     def homer(**kwargs): return Homer(**kwargs)
+    
+    @property
     def hypno(**kwargs): return Hypno(**kwargs)
+    
+    @property
     def ibm(**kwargs): return Ibm(**kwargs)
+    
+    @property
     def iwashi(**kwargs): return Iwashi(**kwargs)
+    
+    @property
     def jellyfish(**kwargs): return Jellyfish(**kwargs)
+    
+    @property
     def karl_marx(**kwargs): return Karl_marx(**kwargs)
+    
+    @property
     def kilroy(**kwargs): return Kilroy(**kwargs)
+    
+    @property
     def king(**kwargs): return King(**kwargs)
+    
+    @property
     def kiss(**kwargs): return Kiss(**kwargs)
+    
+    @property
     def kitten(**kwargs): return Kitten(**kwargs)
+    
+    @property
     def kitty(**kwargs): return Kitty(**kwargs)
+    
+    @property
     def knight(**kwargs): return Knight(**kwargs)
+    
+    @property
     def koala(**kwargs): return Koala(**kwargs)
+    
+    @property
     def kosh(**kwargs): return Kosh(**kwargs)
+    
+    @property
     def lamb(**kwargs): return Lamb(**kwargs)
+    
+    @property
     def lamb2(**kwargs): return Lamb2(**kwargs)
+    
+    @property
     def lightbulb(**kwargs): return Lightbulb(**kwargs)
+    
+    @property
     def lobster(**kwargs): return Lobster(**kwargs)
+    
+    @property
     def lollerskates(**kwargs): return Lollerskates(**kwargs)
+    
+    @property
     def luke_koala(**kwargs): return Luke_koala(**kwargs)
+    
+    @property
     def mailchimp(**kwargs): return Mailchimp(**kwargs)
+    
+    @property
     def maze_runner(**kwargs): return Maze_runner(**kwargs)
+    
+    @property
     def mech_and_cow(**kwargs): return Mech_and_cow(**kwargs)
+    
+    @property
     def meow(**kwargs): return Meow(**kwargs)
+    
+    @property
     def milk(**kwargs): return Milk(**kwargs)
+    
+    @property
     def minotaur(**kwargs): return Minotaur(**kwargs)
+    
+    @property
     def mona_lisa(**kwargs): return Mona_lisa(**kwargs)
+    
+    @property
     def moofasa(**kwargs): return Moofasa(**kwargs)
+    
+    @property
     def mooghidjirah(**kwargs): return Mooghidjirah(**kwargs)
+    
+    @property
     def moojira(**kwargs): return Moojira(**kwargs)
+    
+    @property
     def moose(**kwargs): return Moose(**kwargs)
+    
+    @property
     def mule(**kwargs): return Mule(**kwargs)
+    
+    @property
     def mutilated(**kwargs): return Mutilated(**kwargs)
+    
+    @property
     def nyan(**kwargs): return Nyan(**kwargs)
+    
+    @property
     def octopus(**kwargs): return Octopus(**kwargs)
+    
+    @property
     def okazu(**kwargs): return Okazu(**kwargs)
+    
+    @property
     def owl(**kwargs): return Owl(**kwargs)
+    
+    @property
     def pawn(**kwargs): return Pawn(**kwargs)
+    
+    @property
     def periodic_table(**kwargs): return Periodic_table(**kwargs)
+    
+    @property
     def personality_sphere(**kwargs): return Personality_sphere(**kwargs)
+    
+    @property
     def pinball_machine(**kwargs): return Pinball_machine(**kwargs)
+    
+    @property
     def psychiatrichelp(**kwargs): return Psychiatrichelp(**kwargs)
+    
+    @property
     def psychiatrichelp2(**kwargs): return Psychiatrichelp2(**kwargs)
+    
+    @property
     def pterodactyl(**kwargs): return Pterodactyl(**kwargs)
+    
+    @property
     def queen(**kwargs): return Queen(**kwargs)
+    
+    @property
     def radio(**kwargs): return Radio(**kwargs)
+    
+    @property
     def ren(**kwargs): return Ren(**kwargs)
+    
+    @property
     def renge(**kwargs): return Renge(**kwargs)
+    
+    @property
     def robot(**kwargs): return Robot(**kwargs)
+    
+    @property
     def robotfindskitten(**kwargs): return Robotfindskitten(**kwargs)
+    
+    @property
     def roflcopter(**kwargs): return Roflcopter(**kwargs)
+    
+    @property
     def rook(**kwargs): return Rook(**kwargs)
+    
+    @property
     def sachiko(**kwargs): return Sachiko(**kwargs)
+    
+    @property
     def satanic(**kwargs): return Satanic(**kwargs)
+    
+    @property
     def seahorse(**kwargs): return Seahorse(**kwargs)
+    
+    @property
     def seahorse_big(**kwargs): return Seahorse_big(**kwargs)
+    
+    @property
     def sheep(**kwargs): return Sheep(**kwargs)
+    
+    @property
     def shikato(**kwargs): return Shikato(**kwargs)
+    
+    @property
     def shrug(**kwargs): return Shrug(**kwargs)
+    
+    @property
     def skeleton(**kwargs): return Skeleton(**kwargs)
+    
+    @property
     def small(**kwargs): return Small(**kwargs)
+    
+    @property
     def smiling_octopus(**kwargs): return Smiling_octopus(**kwargs)
+    
+    @property
     def snoopy(**kwargs): return Snoopy(**kwargs)
+    
+    @property
     def snoopyhouse(**kwargs): return Snoopyhouse(**kwargs)
+    
+    @property
     def snoopysleep(**kwargs): return Snoopysleep(**kwargs)
+    
+    @property
     def spidercow(**kwargs): return Spidercow(**kwargs)
+    
+    @property
     def squid(**kwargs): return Squid(**kwargs)
+    
+    @property
     def squirrel(**kwargs): return Squirrel(**kwargs)
+    
+    @property
     def stegosaurus(**kwargs): return Stegosaurus(**kwargs)
+    
+    @property
     def stimpy(**kwargs): return Stimpy(**kwargs)
+    
+    @property
     def sudowoodo(**kwargs): return Sudowoodo(**kwargs)
+    
+    @property
     def supermilker(**kwargs): return Supermilker(**kwargs)
+    
+    @property
     def surgery(**kwargs): return Surgery(**kwargs)
+    
+    @property
     def tableflip(**kwargs): return Tableflip(**kwargs)
+    
+    @property
     def taxi(**kwargs): return Taxi(**kwargs)
+    
+    @property
     def telebears(**kwargs): return Telebears(**kwargs)
+    
+    @property
     def template(**kwargs): return Template(**kwargs)
+    
+    @property
     def threader(**kwargs): return Threader(**kwargs)
+    
+    @property
     def threecubes(**kwargs): return Threecubes(**kwargs)
+    
+    @property
     def toaster(**kwargs): return Toaster(**kwargs)
+    
+    @property
     def tortoise(**kwargs): return Tortoise(**kwargs)
+    
+    @property
     def turkey(**kwargs): return Turkey(**kwargs)
+    
+    @property
     def turtle(**kwargs): return Turtle(**kwargs)
+    
+    @property
     def tux(**kwargs): return Tux(**kwargs)
+    
+    @property
     def tux_big(**kwargs): return Tux_big(**kwargs)
+    
+    @property
     def tweety_bird(**kwargs): return Tweety_bird(**kwargs)
+    
+    @property
     def vader(**kwargs): return Vader(**kwargs)
+    
+    @property
     def vader_koala(**kwargs): return Vader_koala(**kwargs)
+    
+    @property
     def weeping_angel(**kwargs): return Weeping_angel(**kwargs)
+    
+    @property
     def whale(**kwargs): return Whale(**kwargs)
+    
+    @property
     def wizard(**kwargs): return Wizard(**kwargs)
+    
+    @property
     def wood(**kwargs): return Wood(**kwargs)
+    
+    @property
     def world(**kwargs): return World(**kwargs)
+    
+    @property
     def www(**kwargs): return Www(**kwargs)
+    
+    @property
     def yasuna_01(**kwargs): return Yasuna_01(**kwargs)
+    
+    @property
     def yasuna_02(**kwargs): return Yasuna_02(**kwargs)
+    
+    @property
     def yasuna_03(**kwargs): return Yasuna_03(**kwargs)
+    
+    @property
     def yasuna_03a(**kwargs): return Yasuna_03a(**kwargs)
+    
+    @property
     def yasuna_04(**kwargs): return Yasuna_04(**kwargs)
+    
+    @property
     def yasuna_05(**kwargs): return Yasuna_05(**kwargs)
+    
+    @property
     def yasuna_06(**kwargs): return Yasuna_06(**kwargs)
+    
+    @property
     def yasuna_07(**kwargs): return Yasuna_07(**kwargs)
+    
+    @property
     def yasuna_08(**kwargs): return Yasuna_08(**kwargs)
+    
+    @property
     def yasuna_09(**kwargs): return Yasuna_09(**kwargs)
+    
+    @property
     def yasuna_10(**kwargs): return Yasuna_10(**kwargs)
+    
+    @property
     def yasuna_11(**kwargs): return Yasuna_11(**kwargs)
+    
+    @property
     def yasuna_12(**kwargs): return Yasuna_12(**kwargs)
+    
+    @property
     def yasuna_13(**kwargs): return Yasuna_13(**kwargs)
+    
+    @property
     def yasuna_14(**kwargs): return Yasuna_14(**kwargs)
+    
+    @property
     def yasuna_16(**kwargs): return Yasuna_16(**kwargs)
+    
+    @property
     def yasuna_17(**kwargs): return Yasuna_17(**kwargs)
+    
+    @property
     def yasuna_18(**kwargs): return Yasuna_18(**kwargs)
+    
+    @property
     def yasuna_19(**kwargs): return Yasuna_19(**kwargs)
+    
+    @property
     def yasuna_20(**kwargs): return Yasuna_20(**kwargs)
+    
+    @property
     def ymd_udon(**kwargs): return Ymd_udon(**kwargs)
+    
+    @property
     def zen_noh_milk(**kwargs): return Zen_noh_milk(**kwargs)
